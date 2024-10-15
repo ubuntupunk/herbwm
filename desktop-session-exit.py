d138deab640093b1883bc6feabcb4aa752b2a179
ubuntupunk@gmail.com <ubuntupunk@gmail.com>
2024-10-15 11:15:25 +0200

    changes

--- desktop-session-exit.py
+++ desktop-session-exit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 #Name: desktop-session-exit.py
+#Dir: /usr/local/lib/desktop-session/
 #Version: 2.0
 #Depends: python, Gtk
 #Author: Dave (david@daveserver.info)
@@ -16,28 +17,39 @@ import sys
 ButtonWidth = 240
 ButtonHeight = 80
 
-#ICONS = "/usr/share/icons/antiX-numix-bevel"
-#ICONS = "/usr/share/icons/antiX-numix-square"
-ICONS = "/usr/share/icons/antiX-papirus"
-#ICONS = "/usr/share/icons/antiX-faenza"
-#ICONS = "/usr/share/icons/antiX-moka"
+# More robust icon path handling
+try:
+    ICONS = os.path.expanduser("~/.local/share/icons/hicolor/48x48/apps") #Fallback to a common location
+    if not os.path.exists(ICONS):
+        ICONS = "/usr/share/icons/hicolor/48x48/apps" #Fallback to a system location
+except Exception as e:
+    print(f"Error determining icon path: {e}")
+    ICONS = "" #Set to empty string if path cannot be determined
+
 
 gettext.install(domain = "desktop-session-exit", codeset   = 'utf-8')
 
 class mainWindow(Gtk.Window):
     def button_clicked(self, widget, command):
-        os.system(command)
-        sys.exit()
-    
+        try:
+            os.system(command)
+            sys.exit()
+        except Exception as e:
+            print(f"Error executing command: {e}")
+            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, f"Error: {e}")
+            dialog.run()
+            dialog.destroy()
+
     def build_button(self,image,text,command,column,row):
-        #Increase button count by one for use of mnemonic
-        #self.button_loop += 1
-        
         button = Gtk.Button.new_with_mnemonic("_"+_(text))
-        button_image = Gtk.Image.new_from_file(image)
-        button.set_image(button_image)
-        button.set_image_position(Gtk.PositionType.TOP)
-        button.set_always_show_image(True)
+        try:
+            button_image = Gtk.Image.new_from_file(image)
+            button.set_image(button_image)
+            button.set_image_position(Gtk.PositionType.TOP)
+            button.set_always_show_image(True)
+        except Exception as e:
+            print(f"Error loading image {image}: {e}")
+
         button.connect("clicked", self.button_clicked, command)
         button.set_hexpand(True)
         button.set_vexpand(True)
@@ -58,13 +70,10 @@ class mainWindow(Gtk.Window):
         self.add(self.grid)
         self.grid.show()
         
-        #Set first button number for use of mnemonic
-        self.button_loop = 0
         #Build Buttons: ICON, NAME, ACTION, COLUMN, ROW
         self.build_button(ICONS+"/lock.png","Lock Screen","desktop-session-exit -L",1,1)
         self.build_button(ICONS+"/restart.png","Restart Session","desktop-session-exit -R",1,2) 
         self.build_button(ICONS+"/reboot.png","Reboot","desktop-session-exit -r",1,3)
-        #self.build_button(ICONS+"/hibernate.png","Hibernate","desktop-session-exit -H",1,4)
         self.build_button(ICONS+"/logout.png","Log Out","desktop-session-exit -l",2,1)
         self.build_button(ICONS+"/suspend.png","Suspend","desktop-session-exit -S",2,2)
         self.build_button(ICONS+"/shutdown.png","Shutdown","desktop-session-exit -s",2,3)
@@ -72,5 +81,5 @@ class mainWindow(Gtk.Window):
 win = mainWindow()
 win.connect("delete-event", Gtk.main_quit)
 import signal
-signal.signal(signal.SIGINT, signal.SIG_DFL) # without this, Ctrl+C from parent term is ineffectual
+signal.signal(signal.SIGINT, signal.SIG_DFL) 
 Gtk.main()
--- polypanel.sh
+++ polypanel.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
- pkill -q polybar
+pkill -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
+if type "xrandr" &>/dev/null; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload -c "$HOME"/.config/polybar/polybar_4/config mainbar-right & polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-left &\
-        polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-right-1 & polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-right-2 & polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-right-3 &\
-        polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-right-4 & polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-center &
+    MONITOR=$m polybar --reload -c "$HOME/.config/polybar/config.ini" mainbar  &
+    #polybar -c "$HOME/.config/polybar/config.ini" mainbar &
+    #polybar -c "$HOME/.config/polybar/config.ini" mainbar-right-1 &
+    #polybar -c "$HOME/.config/polybar/config.ini" mainbar-right-2 &
+    #polybar -c "$HOME/.config/polybar/config.ini" mainbar-right-3 &
+    #polybar -c "$HOME/.config/polybar/config.ini" mainbar-right-4 &
+    #polybar -c "$HOME/.config/polybar/config.ini" mainbar-center &
   done
 else
-  polybar --reload -c "$HOME"/.config/polybar/polybar_4/config mainbar-right & polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-left & polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-right-1\
-      & polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-right-2 & polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-right-3 & polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-right-4 &\
-      polybar -c "$HOME"/.config/polybar/polybar_4/config mainbar-center &
+  polybar --reload -c "$HOME/.config/polybar/config.ini" mainbar &
+  #polybar -c "$HOME/.config/polybar/config.ini" mainbar-left &
+  #polybar -c "$HOME/.config/polybar/config.ini" mainbar-right-1 &
+  #polybar -c "$HOME/.config/polybar/config.ini" mainbar-right-2 &
+  #polybar -c "$HOME/.config/polybar/config.ini" mainbar-right-3 &
+  #polybar -c "$HOME/.config/polybar/config.ini" mainbar-right-4 &
+  #polybar -c "$HOME/.config/polybar/config.ini" mainbar-center &
 fi
-
-
